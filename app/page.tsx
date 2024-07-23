623c43c27caf6047f28f165818063b6e84228837
Mubeen-Ul-Hassan <mubeenulhassan396@gmail.com>
2024-07-23 22:28:54 +0500

    position fix

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ const Page = () => {
       <Screen count={count} />
       <Button setCount={setCount} count={count} />
       <div className="flex justify-center ">
-        <p className="text-white text-base w-10/12 sm:w-6/12 text-center pt-5">
+        <p className="text-white text-base w-10/12 sm:w-6/12 text-center pt-10">
           Your counter tally is saved to your browser (on your device)
           automatically. If you clear your cache it will be lost. If you would
           like it synced to the cloud you should create a free account.
