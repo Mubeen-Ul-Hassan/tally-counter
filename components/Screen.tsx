9ed57453e5cb06f7c6f64cc0eb1b719338238d2a
Mubeen-Ul-Hassan <mubeenulhassan396@gmail.com>
2024-07-23 22:33:04 +0500

    position fix

--- components/Screen.tsx
+++ components/Screen.tsx
@@ -7,7 +7,7 @@ interface countGuard {
 export default function Screen({ count }: countGuard) {
   return (
     <section className="flex justify-center items-center">
-      <div className="bg-customDarkGray w-full sm:mx-80 sm:rounded-t-xl sm:py-12 mt-6 sm:mt-12 h-72 ms:h-96 flex flex-col justify-center items-center">
+      <div className="bg-customDarkGray w-full sm:mx-80 sm:rounded-t-xl sm:py-12 mt-12 h-72 ms:h-96 flex flex-col justify-center items-center">
         <h1 className="font-semibold text-white">Unnamed Counter</h1>
         <ul className="text-white text-8xl sm:text-10xl font-semibold pt-5 sm:pt-0">
           <li className="text-customGreen">{count}</li>
